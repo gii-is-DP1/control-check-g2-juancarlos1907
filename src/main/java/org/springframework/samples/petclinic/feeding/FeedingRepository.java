00a6e664646fcacf19accba9c7ac5de237006d01
JC <juamorper2@alum.us.es>
2022-01-14 12:22:52 +0100

    Entrega

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
@@ -3,9 +3,15 @@ package org.springframework.samples.petclinic.feeding;
 import java.util.List;
 import java.util.Optional;
 
-public interface FeedingRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FeedingRepository extends CrudRepository{
     List<Feeding> findAll();
-    // List<FeedingType> findAllFeedingTypes();
+    @Query("SELECT p FROM FeedingType p")
+    List<FeedingType> findAllFeedingTypes();
     Optional<Feeding> findById(int id);
     Feeding save(Feeding p);
+    @Query("SELECT f FROM FeedingType f WHERE f.name=?1")
+    FeedingType getFeedingType(String name);
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -1,22 +1,34 @@
 package org.springframework.samples.petclinic.feeding;
-
+import org.springframework.samples.petclinic.pet.Pet;
+import org.springframework.samples.petclinic.pet.PetType;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public class FeedingService {
+
+    private FeedingRepository feedingRepository;
+
     public List<Feeding> getAll(){
         return null;
     }
 
     public List<FeedingType> getAllFeedingTypes(){
-        return null;
+        return feedingRepository.findAllFeedingTypes();
     }
 
     public FeedingType getFeedingType(String typeName) {
         return null;
     }
 
+    @Transactional(rollbackFor = UnfeasibleFeedingException.class)
     public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
+        FeedingType f = p.getFeedingType();
+        PetType a = p.getPet().getType();
+        if(f.getPetType()!=a){
+           throw new UnfeasibleFeedingException();
+        }else{
+            return feedingRepository.save(p);
+        }
     }
 
     
